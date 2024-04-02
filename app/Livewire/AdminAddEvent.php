6309b29075a65f78c92e854830954c0e08998c0b
AngeloWils <awtsotto@gmail.com>
2024-04-03 01:01:24 +0800

    Started on input validation on the events

--- app/Livewire/AdminAddEvent.php
+++ app/Livewire/AdminAddEvent.php
@@ -4,12 +4,19 @@ namespace App\Livewire;
 
 use App\Models\EventModel;
 use Livewire\Attributes\Title;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class AdminAddEvent extends Component
 {
+    #[Validate('required', message: 'Please provide the name of this event')]
     public $name;
+
+    #[Validate('required', message: 'Please provide the date of this event')]
     public $date;
+
+    #[Validate('required',
+        message: 'Please provide the location of this event')]
     public $location;
     public $host;
     public $time;
@@ -17,9 +24,11 @@ class AdminAddEvent extends Component
 
     public function add(): void
     {
+        $this->validate();
+
         EventModel::create([
             'name' => $this->name,
-            'date' => $this->date,
+            'date' => "{$this->date} {$this->time}",
             'location' => $this->location,
             'info' => $this->info,
         ]);
--- resources/views/livewire/admin-add-event.blade.php
+++ resources/views/livewire/admin-add-event.blade.php
@@ -12,8 +12,7 @@
             'label_name' => 'Location',
             'input_name' => 'location',
             'input_type' => 'text',
-            'input_model' => 'location',
-        ],
+            'input_model' => 'location', ],
         [
             'label_name' => 'Host',
             'input_name' => 'host',
@@ -42,42 +41,48 @@
 @endphp
 
 <div class="font['Montserrat'] min-w-screen min-h-screen bg-slate-800 flex
-    flex-col text-xl text-slate-100 text-center py-4">
+    flex-col text-xl text-slate-100 text-center shadow-2xl">
 
-    <div class="pb-4 text-3xl font-black">
-        Add Event
-    </div>
-
-    <form wire:submit="add"
-        class="text-slate-100 font-bold flex flex-col grow justify-around">
-        <div class="flex flex-col max-w-full">
-            @foreach($form_inputs as $form_input)
-            <div class="min-w-full flex">
-                <label for="{{ $form_input['input_name'] }}"
-                    class="{{ $labels_style }}">
-                    {{ $form_input['label_name'] }}
-                </label>
-                <input type="{{ $form_input['input_type'] }}"
-                    wire:model ="{{ $form_input['input_model'] }}"
-                    name="" id="{{ $form_input['input_name'] }}" value=""
-                    class="{{ $inputs_style }}">
+    <div class="flex flex-col justify-center items-center grow min-h-screen">
+        <form wire:submit="add"
+            class="text-slate-100 font-bold flex flex-col grow justify-around
+            lg:w-1/2 bg-slate-700 px-2 sm:px-8">
+        <div class="py-4 text-3xl font-black sm:min-w-screen">
+            Add Event
+        </div>
+            <div class="flex flex-col max-w-full">
+                @foreach($form_inputs as $form_input)
+                <div class="min-w-full flex">
+                    <label for="{{ $form_input['input_name'] }}"
+                        class="{{ $labels_style }}">
+                        {{ $form_input['label_name'] }}
+                    </label>
+                    <input type="{{ $form_input['input_type'] }}"
+                        wire:model ="{{ $form_input['input_model'] }}"
+                        name="" id="{{ $form_input['input_name'] }}" value=""
+                        class="{{ $inputs_style }}">
+                    <div>
+                        @error($form_input['input_model'])
+                        {{ $message }}
+                        @enderror
+                    </div>
+                </div>
+                @endforeach
             </div>
-            @endforeach
-        </div>
-        <div class="flex justify-around min-w-full text-slate-100">
-            <button type="submit"
-                class="border-2 min-w-28 border-green-500 rounded-full font-bold
-                transition ease-out delay-50 hover:bg-green-500
-                hover:text-slate-800">
-                Add
-            </button>
-            <button type="reset"
-                class="border-2 min-w-28 border-red-500 rounded-full font-bold
-                transition ease-out delay-50 hover:bg-red-500
-                hover:text-slate-800">
-                Clear
-            </button>
-        </div>
-    </form>
-
+            <div class="flex justify-around min-w-full py-4 text-slate-100">
+                <button type="submit"
+                    class="border-2 min-w-28 border-green-500 rounded-full font-bold
+                    transition ease-out delay-50 hover:bg-green-500
+                    hover:text-slate-800">
+                    Add
+                </button>
+                <button type="reset"
+                    class="border-2 min-w-28 border-red-500 rounded-full font-bold
+                    transition ease-out delay-50 hover:bg-red-500
+                    hover:text-slate-800">
+                    Clear
+                </button>
+            </div>
+        </form>
+    </div>
 </div>
